581a4851cbee4c17bfaf5a499d2aaee51d9f9635
vladislavkovalskyi <v.darknesssb@gmail.com>
2024-08-08 01:05:10 +0300

    Add solution that automatically appends a dp into dps list in __init__.py files

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,3 +1,14 @@
-from . import start
+import os
+import importlib
 
-dps = [start.dp]
+dps = []
+
+commands_path = os.path.dirname(__file__)
+
+for filename in os.listdir(commands_path):
+    if filename.endswith(".py") and filename != "__init__.py":
+        module_name = filename[:-3]
+        module_full_name = f"src.commands.{module_name}"
+        module = importlib.import_module(module_full_name)
+        if hasattr(module, "dp"):
+            dps.append(module.dp)
--- src/middlewares/__init__.py
+++ src/middlewares/__init__.py
@@ -1,3 +1,14 @@
-from . import context
+import os
+import importlib
 
-dps = [context.dp]
+dps = []
+
+commands_path = os.path.dirname(__file__)
+
+for filename in os.listdir(commands_path):
+    if filename.endswith(".py") and filename != "__init__.py":
+        module_name = filename[:-3]
+        module_full_name = f"src.middlewares.{module_name}"
+        module = importlib.import_module(module_full_name)
+        if hasattr(module, "dp"):
+            dps.append(module.dp)
